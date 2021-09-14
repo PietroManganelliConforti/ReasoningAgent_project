1ee1990aa152f67a5e6ec237d3ab572e6fc14886
IreneBondanza <irene@asc27.com>
2021-09-14 15:31:10 +0200

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# RA_project
+# Reasoning agent projetc: Policy Networks for Non-Markovian Reinforcement Learning Rewards
+
+## Setup
+* Install environment:
+
+`git clone https://github.com/ireneb97/RA_project.git`
+
+`cd RA_project`
+
+* Create and initialize environment:
+
+`python3 -m venv ./venv`
+
+`source venv/bin/activate`
+
+* Install dependencies:
+
+`pip install -r requirements.txt`
+
+## Authors
+
+- Irene Bondanza (bondanza.1747677@studenti.uniroma1.it)
+- Matteo Emanuele (emanuele.1912588@studenti.uniroma1.it)
+- Pietro Manganelli Conforti (manganelliconforti.1754825@studenti.uniroma1.it)
---
+++ models.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+
+class Actor(nn.Module):
+     
+    def __init__(self, state_dim, n_actions, n_colors):
+        super().__init__()
+        self.state_nets = nn.ModuleList([nn.Sequential(
+                        nn.Linear(state_dim, 64),
+                        nn.Tanh(),
+                        nn.Linear(64, 32),
+                        nn.Tanh(),
+                        nn.Linear(32, n_actions),
+                        nn.Softmax(dim=-1)
+        ) for i in range(n_colors)]) 
+
+    def forward(self, X): 
+        return self.state_nets[int(X[-1])](X)
+        
+    def save_model_weights(self, path:str):
+        torch.save(self.state_dict(), path)
+
+    def load_model_weights(self, path:str, device:str='cpu'):
+        self.load_state_dict(torch.load(path, map_location=torch.device(device)))
+
+class Critic(nn.Module):
+
+    def __init__(self, state_dim, n_colors):
+        super().__init__()
+        self.state_nets = nn.ModuleList([nn.Sequential(
+                nn.Linear(state_dim, 64),
+                nn.ReLU(),
+                nn.Linear(64, 32),
+                nn.ReLU(),
+                nn.Linear(32, 1)
+        ) for i in range(n_colors)])
+
+    def forward(self, X):
+        return self.state_nets[int(X[-1])](X) 
+
+    def save_model_weights(self, path:str):
+        torch.save(self.state_dict(), path)
+
+    def load_model_weights(self, path:str, device:str='cpu'):
+        self.load_state_dict(torch.load(path, map_location=torch.device(device)))
